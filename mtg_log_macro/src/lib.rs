e4e3bffb704629eb206ebac08166d623954f8b6b
Elchanan Haas <haas2376@gmail.com>
2023-05-14 20:47:05 -0700

    Finish the logging proc macro implementation

--- mtg_log_macro/src/lib.rs
+++ mtg_log_macro/src/lib.rs
@@ -1,5 +1,5 @@
-use quote;
-use syn::{parse_macro_input,DeriveInput,Data,Fields,Type, FieldsNamed, spanned::Spanned, FieldsUnnamed, DataEnum};
+use quote::{self};
+use syn::{parse_macro_input,DeriveInput,Data,Fields,Type, FieldsNamed, spanned::Spanned, FieldsUnnamed, DataEnum, token::{Token, Comma}, punctuated::Punctuated, Variant};
 
 use proc_macro2::{TokenStream, Ident};
 
@@ -35,7 +35,7 @@ pub fn derive_mtg_log(input: proc_macro::TokenStream) -> proc_macro::TokenStream
                 #vis enum #name #generics #fields
             }
         }
-        _=> todo!("G This data type isn't implemented yet")
+        _=> todo!("This data type isn't implemented yet")
     };
     let implement=impl_mtglog(&name, &pass_ast);
     res.extend(implement.into_iter());
@@ -45,14 +45,89 @@ pub fn derive_mtg_log(input: proc_macro::TokenStream) -> proc_macro::TokenStream
 fn impl_mtglog(name: &Ident, ast:&DeriveInput) -> TokenStream{
     let generics: &syn::Generics=&ast.generics;
     let orig_name=&ast.ident;
+    let body=match ast.data.clone(){
+        Data::Struct(data) => {
+            impl_for_fields(data.fields)
+        },
+        Data::Enum(data)=>{
+            impl_for_enum(data.variants)
+        }
+        _=>unimplemented!()
+    };
     quote::quote!{
         impl #generics MTGLog for #orig_name #generics{
             type LogType = #name;
-            fn log(&self, game_context: GameContext) -> Self::LogType{
-
+            fn log(&self, game_context: &GameContext) -> Self::LogType{
+                #body
             }
         }
     }
+}
+fn impl_for_enum(variants:Punctuated<Variant, Comma>) -> TokenStream{
+    let mut new_code=Vec::new();
+    for variant in variants{
+        let ident=variant.ident;
+        let inner=impl_for_fields_inner(variant.fields);
+        new_code.push(quote::quote!(
+            Self::#ident => {
+                Self::LogType:: #ident #inner
+            }
+        ))
+    }
+    quote::quote!(
+        match self{
+            #(
+                #new_code , 
+            )*
+        }
+    )
+}
+fn impl_for_fields(fields:Fields) -> TokenStream{
+    let inner=impl_for_fields_inner(fields);
+    quote::quote!(Self::LogType #inner)
+}
+fn impl_for_fields_inner(fields:Fields) -> TokenStream{
+    match fields{
+        Fields::Unit => {
+            quote::quote!( () )
+        },
+        Fields::Unnamed(fields) => {
+            impl_for_fields_unnamed(fields)
+        },
+        Fields::Named(fields) => {
+            impl_for_fields_named(fields)
+        }
+        _=>unimplemented!()
+    }
+}
+fn impl_for_fields_named(fields: FieldsNamed) -> TokenStream{
+    let mut new_code=Vec::new();
+    for field in fields.named{
+        let ty=field.ty.clone();
+        let name = field.ident.unwrap();
+        new_code.push(quote::quote_spanned!{ field.ty.span()=>
+            #name : <#ty as MTGLog>::log(game_context)
+        })
+    }
+    quote::quote!(
+        {
+            #( #new_code , )*
+        }
+    )
+}
+fn impl_for_fields_unnamed(fields: FieldsUnnamed) -> TokenStream{
+    let mut new_code=Vec::new();
+    for field in fields.unnamed{
+        let ty=field.ty.clone();
+        new_code.push(quote::quote_spanned!{ field.ty.span()=>
+            <#ty as MTGLog>::log(game_context)
+        })
+    }
+    quote::quote!(
+        (
+            #( #new_code , )*
+        )
+    )
 }
 fn modify_fields(fields:Fields) -> TokenStream{
     match fields {
